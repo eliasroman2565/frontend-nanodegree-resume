f747087a855c136d0bbbde55fd4a916916c18a88
eliasroman2565 <eliasroman@Eliass-MBP.cable.rcn.com>
2016-11-24 09:40:23 -0500

    Update

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,6 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
+var name = "Elias Roman";
+var formattedName = HTMLheaderName.replace("%data%", name);
+var role = "Front-end Developer";
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+$("#header").prepend(formattedRole);
+$("#header").prepend(formattedName);
