e71e7779abc1b6aee2c83465360851ba3a142043
eliasroman2565 <eliasroman@Eliass-MBP.cable.rcn.com>
2016-12-12 21:03:23 -0500

    12/12

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,53 +1,45 @@
 
-//var formattedName = HTMLheaderName.replace("%data%", name);
-//var formattedRole = HTMLheaderRole.replace("%data%", role);
-//$("#header").prepend(formattedRole);
-//$("#header").prepend(formattedName);
 var bio = {
-  "name" : "Elias David Roman",
-  "Role" : "Front-End Web Developer",
-  "welcomeMessage" : "Hello, this is my resume.",
-  "contacts" : ["Email: elias.roman2565@gmail.com", "Mobile: 570-452-1873", "GitHub: eliasroman2565", "Location: Allentown, PA"],
-  "skills" : ["HTML", "CSS", "JavaScript", "Microsoft Office"],
-  ""
+  "name": "Elias David Roman",
+  "Role": "Front-End Web Developer",
+  "welcomeMessage": "Hello, this is my resume.",
+  "contacts": ["Email: elias.roman2565@gmail.com", "Mobile: 570-452-1873", "GitHub: eliasroman2565", "Location: Allentown, PA"],
+  "skills": ["HTML", "CSS", "JavaScript", "Microsoft Office"]
 };
 
 var education = {
   "Schools" : {
 
-      "Name" : "Penn College",
-      "City" : "Williamsport, PA",
-      "Degree" : "Not Complete",
-      "Major" : "Civil Engineering"
+      "Name": "Penn College",
+      "City": "Williamsport, PA",
+      "Degree": "Not Complete",
+      "Major": "Civil Engineering"
     },
 
   "onlineCourses" : {
 
-      "Name" : "Udacity",
-      "Course" : "Front-End Web Developer"
+      "Name": "Udacity",
+      "Course": "Front-End Web Developer"
     }
 
 };
 
 var work = {
-  "work" : [
-    {
-      "Company" : "Horgan Brothers Inc.",
-      "Title" : "Drafting Technician",
-      "Location" : "Harleysville, PA",
-      "Dates" : "October, 2016 - Present"
-    },
-    {
-      "Company" : "Wagman Heavy Civil",
-      "Title" : "Surveyor",
-      "Location" : "Allentown, PA",
-      "Dates" : "January, 2016 - October, 2016"
-    }
-  ]
+  "jobs": [{
+      "employer": "Horgan Brothers Inc.",
+      "title": "Drafting Technician",
+      "dates": "October, 2016 - Present",
+      "description": "Digitize contours to perform accurate takeoff of earthwork and materials needed to complete the project."
+    }, {
+      "employer": "Wagman Heavy Civil",
+      "title": "Surveyor",
+      "dates": "January, 2016 - October, 2016",
+      "description": "Performed construction stake-out for structures, superstructures, footers, etc. Performed topographic surveys which allowed for accurate takeoff of quantities for payment by owner. Performed as-built surveys of pipes and structures for future referece."
+    }]
 };
 
 var formattedName = HTMLheaderName.replace("%data%", bio.name);
-$("#header").prepend(formattedName);
+  $("#header").prepend(formattedName);
 
 if(bio.skills.length > 0) {
   $("#header").append(HTMLskillsStart);
@@ -60,4 +52,21 @@ if(bio.skills.length > 0) {
   $("#skills").append(formattedSkill);
   var formattedSkill = HTMLskills.replace("%data%", bio.skills[3]);
   $("#skills").append(formattedSkill);
+};
+
+for (job in work.jobs) {
+  $("#workExperience").append(HTMLworkStart);
+
+  var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+  var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+  var formattedEmployerTitle = formattedEmployer + formattedTitle;
+
+  $(".work-entry:last").append(formattedEmployerTitle);
+
+  var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates)
+    $(".work-entry:last").append(formattedDates);
+
+  var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+    $(".work-entry:last").append(formattedDescription);
+
 };
