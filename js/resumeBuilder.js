053e9b158bba8fb943309a781c2a0e84bc64cd55
eliasroman2565 <elias.roman2565@gmail.com>
2016-12-16 13:57:59 -0500

    12/16

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -4,22 +4,22 @@ var bio = {
   "Role": "Front-End Web Developer",
   "welcomeMessage": "Hello, this is my resume.",
   "contacts": ["Email: elias.roman2565@gmail.com", "Mobile: 570-452-1873", "GitHub: eliasroman2565", "Location: Allentown, PA"],
-  "skills": ["HTML", "CSS", "JavaScript", "Microsoft Office"]
+  "skills": ["HTML", "CSS", "JavaScript", "Microsoft Office"],
 };
 
 var education = {
   "Schools" : {
 
-      "Name": "Penn College",
-      "City": "Williamsport, PA",
-      "Degree": "Not Complete",
-      "Major": "Civil Engineering"
+      "name": "Penn College",
+      "location": "Williamsport, PA",
+      "degree": "Not Complete",
+      "major": "Civil Engineering"
     },
 
   "onlineCourses" : {
 
-      "Name": "Udacity",
-      "Course": "Front-End Web Developer"
+      "name": "Udacity",
+      "course": "Front-End Web Developer"
     }
 
 };
@@ -28,16 +28,31 @@ var work = {
   "jobs": [{
       "employer": "Horgan Brothers Inc.",
       "title": "Drafting Technician",
+      "location" : "Harleysville, PA",
       "dates": "October, 2016 - Present",
       "description": "Digitize contours to perform accurate takeoff of earthwork and materials needed to complete the project."
     }, {
       "employer": "Wagman Heavy Civil",
       "title": "Surveyor",
+      "location" : "Allentown, PA",
       "dates": "January, 2016 - October, 2016",
       "description": "Performed construction stake-out for structures, superstructures, footers, etc. Performed topographic surveys which allowed for accurate takeoff of quantities for payment by owner. Performed as-built surveys of pipes and structures for future referece."
     }]
 };
 
+var projects = {
+  "projects" : [{
+    "title" : "Portfolio",
+    "dates" : "September, 2016 - October, 2016",
+    "description" : "I created a static HTML/CSS webpage which displayed web development projects that I have worked on.",
+    "images" : "http://www.illuminessensce.com/wp-content/uploads/2012/12/Image-Coming-Soon-Placeholder.png"
+  }, {
+    "title" : "Resume",
+    "dates" : "October, 2016 - December, 2016",
+    "description" : "I created a webpage utilizing HTML/CSS and JavaScript which displayed my work history, contact information, educational history, etc.",
+    "images" : "http://www.illuminessensce.com/wp-content/uploads/2012/12/Image-Coming-Soon-Placeholder.png"
+  }]
+};
 
 
 var formattedName = HTMLheaderName.replace("%data%", bio.name);
@@ -72,22 +87,33 @@ function displayWork() {
     var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
       $(".work-entry:last").append(formattedDescription);
 
+      var formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
+      $(".work-entry:last").append(formattedLocation);
+
   };
 }
-
 displayWork();
 
-function inName(name) {
-  name = name.trim().split(" ");
-  console.log(name);
-  name[1] = name[1].toUpperCase();
-  name[0] = name[0].slice(0,1).toUpperCase() +
-  name[0].slice(1).toLowerCase();
+projects.display = function() {
+  for (project in projects.projects) {
+      $("#projects").append(HTMLprojectStart);
 
-  return name[0] + " " + name[1];
+      var formattedTitle = HTMLprojectTitle.replace("%data%", projects.projects[project].title);
+      $(".project-entry:last").append(formattedTitle);
+
+      var formattedDates = HTMLprojectDates.replace("%data%", projects.projects[project].dates);
+      $(".project-entry:last").append(formattedDates);
+
+      var formattedDescription = HTMLprojectDescription.replace("%data%", projects.projects[project].description);
+      $(".project-entry:last").append(formattedDescription);
+
+      if (projects.projects[project].images.length > 0) {
+        for (image in projects.projects[project].images) {
+          var formattedImage = HTMLprojectImage.replace("%data%", projects.projects[project].images[image]);
+          $(".project-entry:last").append(formattedImage);
+        };
+      };
+  };
 };
-$("#main").append(internationalizeButton);
-
-projects.display = funtction() {
-
-}
+projects.display();
+$("#mapDiv").append(googleMap);
